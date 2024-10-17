b80003fcd5f834a7b512ca2c7b81665e3244d18b
Benjamin Dass <65924137+ben-dass@users.noreply.github.com>
2024-10-17 12:39:35 -0400

    Fixed vite error

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { AliasOptions, defineConfig } from "vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [react()],
@@ -11,9 +11,6 @@ export default defineConfig({
 			"@features": path.resolve(__dirname, "./src/features"),
 			"@app": path.resolve(__dirname, "./src/app"),
 			"@src": path.resolve(__dirname, "./src"),
-		} as AliasOptions,
-	},
-	esbuild: {
-		tsconfigRaw: path.resolve(__dirname, "./tsconfig.app.json"),
+		},
 	},
 });
