b706b06ddfb9051492633157e94878bbef8c280a
Benjamin Dass <benrdass@gmail.com>
2024-10-17 12:35:10 -0400

    fixed shadcn installation path

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { AliasOptions, defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
 			"@components": path.resolve(__dirname, "./src/components"),
-			"@utils": path.resolve(__dirname, "./src/utils"),
+			"@lib": path.resolve(__dirname, "./src/lib"),
 			"@features": path.resolve(__dirname, "./src/features"),
 			"@app": path.resolve(__dirname, "./src/app"),
 			"@src": path.resolve(__dirname, "./src"),
-		},
+		} as AliasOptions,
+	},
+	esbuild: {
+		tsconfigRaw: path.resolve(__dirname, "./tsconfig.app.json"),
 	},
 });
